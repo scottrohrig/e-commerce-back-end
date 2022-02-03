733148237e3108f55d40858aef950c3141a43eaa
scottrohrig <scott.rohrig@gmail.com>
2022-02-03 12:45:18 -0800

    add columns

---
+++ models/ProductTag.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require( 'sequelize' );
+
+const sequelize = require( '../config/connection' );
+
+class ProductTag extends Model { }
+
+ProductTag.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'product_id',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag_id',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
